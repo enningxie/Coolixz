1062c274eb834a036e91648aa2cee85e66ca6fff
enningxie <enningxie@163.com>
2017-12-19 20:20:36 +0800

    x

--- Python/fluent_python/ch01/test1_1.py
+++ Python/fluent_python/ch01/test1_1.py
@@ -1,6 +1,9 @@
+"""
+展示__getitem__和__len__这两个特殊方法
+"""
 import collections
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
+Card = collections.namedtuple('Card', ['rank', 'suit'])  # 构建了一个简单的类来表示一张纸牌
 
 
 class FrenchDeck:
@@ -8,12 +11,20 @@ class FrenchDeck:
     suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self):
-        self._cards = [Card(rank, suit) for rank in self.ranks for suit in self.suits]
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
 
     def __getitem__(self, position):
         return self._cards[position]
+
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
 
 
---
+++ Python/fluent_python/ch01/test1_2.py
@@ -0,0 +1,25 @@
+#  一个简单的二维向量类
+from math import hypot
+
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return "Vector(%r, %r)" % (self.x, self.y)  # %r相比较%s会展示变量的原始数据
+
+    def __abs__(self):
+        return hypot(self.x, self.y)  # 返回欧式距离
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
