cce02559e06b0c78ac59e1a0e5e49b87e0944bda
enningxie <enningxie@163.com>
2017-12-20 18:05:41 +0800

    x

---
+++ Python/test.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+
+
+if __name__ == '__main__':
+    a = Solution()
+    print(a.twoSum([3, 2, 4], 6))
